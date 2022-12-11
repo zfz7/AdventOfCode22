2f7f34d0c4c0bbac161959d0939af94e209a0369
Daniel Eichman <d.eichman@outlook.com>
2022-12-11 07:33:00 -0800

    Day11: parse input file

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,102 +1,27 @@
-val m0 = Monkey(
-    mutableListOf(79, 98),
-    { item: Long -> item * 19 },
-    23,
-    2,
-    3
-)
-val m1 = Monkey(
-    mutableListOf(54, 65, 75, 74),
-    { item: Long -> item + 6 },
-    19,
-    2,
-    0
-)
-val m2 = Monkey(
-    mutableListOf(79, 60, 97),
-    { item: Long -> item * item },
-    13,
-    1,
-    3
-)
-val m3 = Monkey(
-    mutableListOf(74),
-    { item: Long -> item + 3 },
-    17,
-    0,
-    1
-)
-
 fun main() {
-
-    println(day11A(testMonkeys()))
     println(day11A(parseMonkeys(readFile("Day11"))))
-    println(day11B(testMonkeys()))
     println(day11B(parseMonkeys(readFile("Day11"))))
 }
 
-fun testMonkeys(): List<Monkey> =listOf<Monkey>(m0, m1, m2, m3).map{ mk -> mk.copy(items = mk.items.map {it}.toMutableList()) }
-
-fun parseMonkeys(input: String): List<Monkey> {
-    val m0 = Monkey(
-        mutableListOf(66, 71, 94),
-        { item: Long -> item * 5 },
-        3,
-        7,
-        4
-    )
-    val m1 = Monkey(
-        mutableListOf(70),
-        { item: Long -> item + 6 },
-        17,
-        3,
-        0
-    )
-    val m2 = Monkey(
-        mutableListOf(62, 68, 56, 65, 94, 78),
-        { item: Long -> item + 5 },
-        2,
-        3,
-        1
-    )
-    val m3 = Monkey(
-        mutableListOf(89, 94, 94, 67),
-        { item: Long -> item + 2 },
-        19,
-        7,
-        0
-    )
-    val m4 = Monkey(
-        mutableListOf(71, 61, 73, 65, 98, 98, 63),
-        { item: Long -> item * 7 },
-        11,
-        5,
-        6
-    )
-    val m5 = Monkey(
-        mutableListOf(55, 62, 68, 61, 60),
-        { item: Long -> item + 7 },
-        5,
-        2,
-        1
-    )
-    val m6 = Monkey(
-        mutableListOf(93, 91, 69, 64, 72, 89, 50, 71),
-        { item: Long -> item + 1 },
-        13,
-        5,
-        2
-    )
-    val m7 = Monkey(
-        mutableListOf(76, 50),
-        { item: Long -> item * item },
-        7,
-        4,
-        6
-    )
-    return listOf(m0, m1, m2, m3, m4, m5, m6, m7)
-}
-
+fun parseMonkeys(input: String): List<Monkey>
+     = input.split("\n\n").map { mk ->
+        Monkey(
+            items = mk.split("\n").filter{ it.contains("Starting items: ")}[0].split("  Starting items: ",", ").mapNotNull{it.toLongOrNull()}.toMutableList(),
+            op = { item: Long ->
+                val line = mk.split("\n").filter{ it.contains("Operation: new = ")}[0].split(" ")
+                when(line[6]){
+                    "+" -> item + (line[7].toLongOrNull() ?: item)
+                    "-" -> item - (line[7].toLongOrNull() ?: item)
+                    "/" -> item / (line[7].toLongOrNull() ?: item)
+                    "*" -> item * (line[7].toLongOrNull() ?: item)
+                    else -> throw Exception("help")
+                }
+            },
+            divider = mk.split("\n").filter{ it.contains("Test: divisible by ")}[0].split("Test: divisible by ")[1].toLong(),
+            pass = mk.split("\n").filter{ it.contains("If true: throw to monkey ")}[0].split("If true: throw to monkey ")[1].toInt(),
+            fail = mk.split("\n").filter{ it.contains("If false: throw to monkey ")}[0].split("If false: throw to monkey ")[1].toInt(),
+        )
+    }
 fun day11A(monkeys: List<Monkey>): Long {
     repeat(20) {
         monkeys.forEach {
